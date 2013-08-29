5dc6ecc207b2d6edb9a3c82a54552e6675be93f9
angleman <angleman@cormea.com>
2013-08-29 16:08:36 -0600

    add _intTo64fast test

--- test/_intTo64fast.js
+++ test/_intTo64fast.js
@@ -1,14 +1,37 @@
 // test/main.js
-/*var should = require('should');
+var should = require('should');
 var i64    = require('../i64.js');
 
 describe('i64._intTo64fast()', function() {
     describe('no arguments', function() {
-        it('returns 0', function() {
-            var result = i64._intTo64fast();
-            should.exist(result);
-            result.should.equal('0');
+        it('throws exception', function() {
+            var failed = false;
+            try {
+                var result = i64._intTo64fast();
+            } catch(err) {
+                failed = true;
+            }
+            failed.should.equal(true);
+        });
+    });
+
+    describe('negative number', function() {
+        it('throws exception', function() {
+            var failed = false;
+            try {
+                var result = i64._intTo64fast(-22);
+            } catch(err) {
+                failed = true;
+            }
+            failed.should.equal(true);
+        });
+    });
+
+    describe('valid integer', function() {
+        it('returns cR', function() {
+            var result = i64._intTo64fast(821);
+            result.should.equal('cR');
         });
     });
 });
-*/
+
