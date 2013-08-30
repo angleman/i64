821bfff8f718c45e248324df5d1b9b0a4e25bd40
angleman <angleman@cormea.com>
2013-08-29 18:24:56 -0600

    add cross type testing

---
+++ test/intToHex.js
@@ -0,0 +1,19 @@
+var should = require('should')
+  , i64    = require('../i64.js')
+  , a64    = i64.new()
+;
+
+
+
+describe('cross conversion', function() {
+    describe('hexidecimal to integer', function() {
+        it('returns 255', function() {
+            var result = a64.asHex('ff');
+            console.log(result);
+            should.exist(result);
+
+            result = a64.asInt();
+            result.should.equal(255);
+        });
+    });
+});
