56ea4c8ce34002f807b0ac1713d59af6731b46bd
angleman <angleman@cormea.com>
2013-08-29 20:29:06 -0600

    add asDate and tests

--- i64.js
+++ i64.js
@@ -124,13 +124,15 @@ i64.prototype.isI64 = function(i64string) {
 
 
 i64.prototype.valueOf = function(i64string) {
-	if (i64string) {
+	if (typeof i64string == 'undefined') { // no parameter, return the Int64String value
+		return this._value;
+	} else {
 		if (!this.isI64(i64string)) {
 			throw new I64StringError("Invalid valueOf() Int64String");
 		}
 		this._value = i64string;
+		return this;
 	}
-	return this._value;
 }
 
 
@@ -142,26 +144,82 @@ i64.prototype.asInt = function(intvalue) {
 		} else {
 			return this._toIntFast(this._value);
 		}
-	} else { // assign the integer value and return the Int64String
+	} else { // assign the integer value
 		if (typeof intvalue == 'string') {
 			this._value = int_encoder.encode(intvalue, 10);
 		} else {
 			this._value = this._intTo64Fast(intvalue);
 		}
-		return this._value; // return the Int64String
+		return this;
 	}
 }
 
 
 
 i64.prototype.asHex = function(hexvalue) {
-	if (typeof hexvalue == 'undefined') { // no parameter, return the integer value
+	if (typeof hexvalue == 'undefined') { // no parameter, return the hexidecimal value
 		return int_encoder.decode(this._value, 16);
-	} else { // assign the integer value and return the Int64String
+	} else { // assign the hexidecimal value
 		this._value = int_encoder.encode(hexvalue, 16);
-		return this._value; // return the Int64String
+		return this; 
 	}
 }
+
+
+
+i64.prototype._dateToBase64 = function(date, options) {
+	options       = options || this._config;
+    var base_year = (options.base_year) ? options.base_year : 2010
+      , date      = (date)              ? date              : new Date()
+      , dat       = new Date(date)
+      , alphabet  = this._config.alphabet
+      , year      = alphabet.charAt(dat.getFullYear() - base_year)
+      , month     = alphabet.charAt(dat.getMonth())
+      , day       = alphabet.charAt(dat.getDate())
+      , hour      = alphabet.charAt(dat.getHours())
+      , minute    = alphabet.charAt(dat.getMinutes())
+      , second    = alphabet.charAt(dat.getSeconds())
+      , millis    = date.getMilliseconds()
+      , millis1   = Math.floor(millis / 64)
+      , millis2   = millis - (millis1 * 64)
+      , millisa   = alphabet.charAt(millis1)
+      , millisb   = alphabet.charAt(millis2)
+      , result    = year + month + day + hour + minute + second + millisa + millisb
+    ;
+    return result;
+}
+
+
+
+i64.prototype._base64ToDate = function(base64, options) {
+	options       = options || this._config;
+    var base_year = (options.base_year) ? options.base_year : 2010
+      , alphabet  = this._config.alphabet
+      , year      = (base64.length > 0) ? alphabet.indexOf(base64[0]) + base_year : base_year
+      , month     = (base64.length > 1) ? alphabet.indexOf(base64[1]) : 1
+      , day       = (base64.length > 2) ? alphabet.indexOf(base64[2]) : 1
+      , hours     = (base64.length > 3) ? alphabet.indexOf(base64[3]) : 0
+      , minutes   = (base64.length > 4) ? alphabet.indexOf(base64[4]) : 0
+      , seconds   = (base64.length > 5) ? alphabet.indexOf(base64[5]) : 0
+      , millis    = (base64.length > 7) ? alphabet.indexOf(base64[6]) * 64 + alphabet.indexOf(base64[7]) : 0
+      , date      = new Date(year, month, day, hours, minutes, seconds, millis)
+    ;
+    return date;
+}
+
+
+
+i64.prototype.asDate = function(datevalue, options) {
+	options = options || this._config;
+	if (typeof datevalue == 'undefined') { // no parameter, return the datevalue value
+		return this._base64ToDate(this._value, options);
+	} else { // assign the date value
+		this._value = this._dateToBase64(datevalue, options);
+		return this;
+	}
+}
+
+
 
 /** TODO:
 i64.asMicrotime([microtime])      // get/set value as Microtime
@@ -170,7 +228,6 @@ i64.asGeo([degrees, [precision]]) // get/set value as Longitude or Latitude degr
 i64.asGeo(-28.22).as64()      // g2     geo degrees to base64
 
     "base_year":        2010      // to help be human readable. ex: year 0=2010, 1=2011, etc
-  , "date_format":      'ymdhis'  // one character for each date granularity
   , "geo_precision":    2         // approximately 10km (a little more than one decimal point of precision)
 
 **/
---
+++ test/asDate.js
@@ -0,0 +1,28 @@
+var should = require('should')
+  , i64    = require('../i64.js')
+  , a64    = i64.new()
+;
+
+
+
+describe('asDate()', function() {
+    describe('select date', function() {
+        var result;
+        it('returns "38tkcf00"', function() {
+            var date = new Date(2013, 08, 29, 20, 12, 15, 0);
+            result = a64.asDate(date);
+            should.exist(result);
+            result = a64.valueOf();
+            console.log(result);
+            result.should.equal('38tkcf00');
+        });
+
+        it('returns "2013-09-30T02:12:15.000Z"', function() {
+            result = a64.asDate();
+            should.exist(result);
+            result = result.toISOString();
+            console.log(result);
+            result.should.equal('2013-09-30T02:12:15.000Z');
+        });
+    });
+});
